106a4115118af24a19879e47ca3902dc8e15dc08
Andrei Polkanov <andrey.polkanoff@gmail.com>
2023-04-25 02:54:16 +0300

    FIX: after setting unsupported manual status (e.g. FAILED instead of FAIL), exp is corrupted, and it is not possible to set or remove status (only with illegal surgery on exp._data fields). Add checking for the status before assigning or smth like that.
    DIRTY FIX:
    BUG: Proj has status `DONE`, but should be `IN_PROGRESS`:
      ```
      # xman.proj.make_group('Tokenization', 'Try several tokenization technics')
      # xman.proj.group(1).make_exp('Default', 'Default settings: just split on words, vector_size=200, min_count=5, window=5')
      # xman.proj.group(1).make_exp('Punctuation', 'Remove punctuation')
      # xman.proj.group(1).make_exp('Stopwords', 'Remove stopwords (and punctuation)')
      # xman.proj.group(1).make_exp('Lemmatization', 'Lemmatize words (after removing stopwords and punctuation)')
      # xman.proj.group(1).make_exp('Lemmatization-2', 'Lemmatize words with nltk after fail with spacy (with removing stopwords and punctuation)')
      xman.proj.group(1).make_exp('Lowercase', 'Lowercase input')
      xman.proj
      ```
      ```
      Proj [DONE] hw_3-1 - https://stepik.org/lesson/940785/step/1?unit=946946
    
        Group 1 [IN_PROGRESS] Tokenization - Try several tokenization technics
            Exp 1 [SUCCESS *] Default - Default settings: just split on words, vector_size=200, min_count=5, window=5
            Exp 2 [SUCCESS *] Punctuation - Remove punctuation
            Exp 3 [SUCCESS *] Stopwords - Remove stopwords (and punctuation)
            Exp 4 [FAIL *] Lemmatization - Lemmatize words (after removing stopwords and punctuation)
            Exp 5 [FAIL *] Lemmatization-2 - Lemmatize words with nltk after fail with spacy (with removing stopwords and punctuation)
            Exp 6 [EMPTY] Lowercase - Lowercase input
      ```

--- test/test_.py
+++ test/test_.py
@@ -11,16 +11,17 @@ import xman
 from xman import util
 
 
-def make_proj_from_nothing(location_dir=None, name=None, descr=None):
-    if location_dir is None:
-        # location_dir = './gitignore/testproj'  # if running by `pytest` in terminal
-        location_dir = '../gitignore/testproj'
+def make_proj_from_nothing(proj_dir=None, name=None, descr=None):
+    if proj_dir is None:
+        # proj_dir = './gitignore/testproj'  # if running by `pytest` in terminal
+        proj_dir = '../gitignore/testproj'
     if name is None:
         name = 'Test Proj'
     if descr is None:
         descr = 'Test proj descr'
-    shutil.rmtree(location_dir)
-    return xman.make_proj(location_dir, name, descr)
+    if os.path.exists(proj_dir):
+        shutil.rmtree(proj_dir)
+    return xman.make_proj(proj_dir, name, descr)
 
 
 def make_group_from_nothing(name=None, descr=None, num=None):
@@ -60,4 +61,47 @@ def test__bug__getting_tenth_exp_with_dotted_num():
     assert xman.proj.group(1).exp(10) is exp
 
 
+def test__bug__exp_broken_after_setting_wrong_status():
+    exp = make_exp_from_nothing()
+    with pytest.raises(ValueError, match="doesn't have status"):
+        exp.set_manual_status('FAILED', "There's no `FAILED` status - should be `FAIL`")
+    exp.info()
+    assert exp.status.status == 'EMPTY'
+    exp.set_manual_status('FAIL', "Acceptable status")
+    assert exp.status.status == 'FAIL'
 
+
+def test__bug__exp_struct_box_set_manual_status_doesnt_work_after_exp_wrong_status_setting():
+    exp = make_exp_from_nothing()
+    xman.proj.info()
+    try:
+        exp.set_manual_status('FAILED', "There's no `FAILED` status - should be `FAIL`")
+    except:
+        pass
+    xman.proj.set_manual_status('TODO', None)
+    assert xman.proj.status.status == 'TODO'
+    xman.proj.group(1).set_manual_status('TODO', None)
+    assert xman.proj.group(1).status.status == 'TODO'
+
+
+def test__bug__wrong_proj_status():
+    proj = make_proj_from_nothing()
+    group = proj.make_group('Tokenization', 'Try several tokenization technics')
+    group.make_exp('Default', 'Default settings: just split on words, vector_size=200, min_count=5, window=5')
+    group.make_exp('Punctuation', 'Remove punctuation')
+    group.make_exp('Stopwords', 'Remove stopwords (and punctuation)')
+    group.make_exp('Lemmatization', 'Lemmatize words (after removing stopwords and punctuation)')
+    group.make_exp('Lemmatization-2', 'Lemmatize words with nltk after fail with spacy (with removing stopwords and punctuation)')
+    group.exp(1).set_manual_status('SUCCESS', 'Success')
+    group.exp(2).set_manual_status('SUCCESS', 'Success')
+    group.exp(3).set_manual_status('SUCCESS', 'Success')
+    group.exp(4).set_manual_status('FAIL', 'Fail')
+    group.exp(5).set_manual_status('FAIL', 'Fail')
+    group.make_exp('Lowercase', 'Lowercase input')
+    assert group.status.status == 'IN_PROGRESS'
+    assert proj.status.status == 'IN_PROGRESS'
+
+    # TODO There are 2 issues:
+    #  1: [DIRTY HACKED in ExpStructBox._make_child/_remove_child] The group status wasn't updated after creating an exp - calling group.info() before
+    #       getting the status gives the correct status;
+    #  2: [TODO] Getting status is not from @property with updating on each call.
