118cb30287c7b82b12e421fb6a22c20fa4b800b6
Andrei Polkanov <andrey.polkanoff@gmail.com>
2023-06-04 22:33:11 +0300

    requirements.txt and changes in setup.py

--- setup.py
+++ setup.py
@@ -1,42 +1,22 @@
 from setuptools import setup
+from xman import __version__
+
+with open('src/requirements.txt') as f:
+    requirements = f.read().splitlines()
 
 setup(
-    name='xman',
-    version='0.1.0',
+    name='xMan',
+    version=__version__,
     packages=['xman'],
     package_dir={'': 'src'},
     url='https://github.com/wolfhoundgelert/xman',
     license='BSD 3-clause',
     author='Andrei Polkanov',
     author_email='andrey.polkanoff@gmail.com',
-    description='Experiment Manager (xman) [IN_PROGRESS]'
+    description='xMan - Experiment Manager, python library for effective management and organising '
+                'experiments on thе Google Colab platform with sharing and running in parallel '
+                'across different Colab accounts.',
+    install_requires=requirements,
 )
 
 # TODO https://github.com/pypi/support/issues/2738
-
-
-
-# TODO >>>>> support setting from setup.py
-    # import re
-    #
-    # def get_current_version():
-    #     with open('xman/__init__.py', 'r') as f:
-    #         version_file = f.read()
-    #     version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
-    #     if version_match:
-    #         return version_match.group(1)
-    #     raise RuntimeError("Unable to find version string in __init__.py")
-    #
-    # def get_updated_version():
-    #     current_version = get_current_version()
-    #     major, minor, patch = current_version.split('.')
-    #     patch = int(patch) + 1
-    #     return f"{major}.{minor}.{patch}"
-    #
-    # from setuptools import setup
-    #
-    # setup(
-    #     name='xman',
-    #     version=get_updated_version(),
-    #     ...
-    # )
